74ccfef1690ad7d33ef88df87ab83cd8bffa6471
Ann Sarviro <Annka_nuha@i.ua>
2022-05-20 02:27:56 +0300

    general script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './css/styles.css';
-import './js/fetchCountries.js'
+import fetchCountries from './js/fetchCountries.js';
+var debounce = require('lodash.debounce');
+
+
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    inputEl: document.querySelector('#search-box'),
+    ulEl: document.querySelector('.country-list'),
+    boxEl: document.querySelector('.country-info'),
+}
+
+refs.inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+
+function onSearch (event) {
+    event.preventDefault();
+    const searchValue = refs.inputEl.value.trim();
+    if (!searchValue) {return console.log("введите данные")}
+    fetchCountries(searchValue)
+    .then((data) =>{
+        if (data.length>= 10)
+        {return console.log("точнее запрос")}
+
+        if (data.length>1){
+         return renderCountriesList(data);
+        }
+
+        renderCountryCard(data)
+    } 
+    ).catch(error => console.log("не нашли"));
+
+}
+
+function renderCountriesList() {
+    console.log('a lot of Countries')
+}
+
+function renderCountryCard() {
+    console.log('one Country')
+}
